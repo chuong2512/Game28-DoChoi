5dfe602491f31fa855a4cca2b2df9cb1d2be8c0f
chuong2512 <hieutg244@gmail.com>
2023-03-07 21:51:45 +0700

    initial commit

---
+++ Assets/_App/Scripts/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using SoudToy;
+using UnityEngine;
+
+namespace Assets.BanSung.Scripts
+{/// <summary>
+ /// 
+ /// </summary>
+    public class AudioManager : Singleton<AudioManager>
+    {
+        public AudioSource _AudioSource;
+
+        public void Play(AudioClip clip)
+        {
+            _AudioSource.PlayOneShot(clip);
+        }
+    }
+}
